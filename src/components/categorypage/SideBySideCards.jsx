2f9dcea4850675bbc9faf8f88a8b0dfba87dab26
Aleyna Şebnem <sebnemucak.a@gmail.com>
2025-03-28 17:34:56 +0300

    Update SideBySideCards.jsx

--- src/components/categorypage/SideBySideCards.jsx
+++ src/components/categorypage/SideBySideCards.jsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useLanguage } from "../../context/LanguageContext"; // useLanguage kullanıyoruz
+import { useLanguage } from "../../context/LanguageContext";
 
 const SideBySideCards = () => {
-  const { data, language } = useLanguage(); // useLanguage hook'u ile data ve language alıyoruz
+  const { data, language } = useLanguage();
   const { sideBySideCards } = data;
-  const { category } = useParams(); // URL'den kategori bilgisi al
+  const { category } = useParams();
 
   const categoryData = sideBySideCards.find((item) => item.category === category);
 
@@ -13,7 +13,7 @@ const SideBySideCards = () => {
   }
 
   return (
-    <div className="w-full mx-auto py-10 space-y-10">
+    <div className="w-full mx-auto py-10 space-y-10 touch-manipulation">
       <h2 className="text-3xl font-semibold text-left mb-8 text-gray-800">
         {language === "tr" ? "Neden Bu Teknoloji?" : "Why This Technology?"}
       </h2>
@@ -21,10 +21,9 @@ const SideBySideCards = () => {
         {categoryData.left.map((leftItem, index) => (
           <div
             key={index}
-            className="w-4/5 grid grid-cols-1 md:grid-cols-2 gap-8 items-stretch border-b border-gray-800 pb-10 mb-10 ml-auto"
+            className="w-4/5 grid grid-cols-1 md:grid-cols-2 gap-8 items-stretch border-b border-gray-800 pb-10 mb-10 ml-auto will-change-transform"
           >
-            {/* Sol Kart */}
-            <div className="bg-white p-6 shadow-lg rounded-lg flex flex-col h-full text-left transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+            <div className="bg-white p-6 shadow-lg rounded-lg flex flex-col h-full text-left transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
               <h2 className="text-2xl font-bold text-gray-900 mb-2">
                 {leftItem.title}
               </h2>
@@ -34,8 +33,7 @@ const SideBySideCards = () => {
               <p className="text-sm text-gray-600 mt-auto">{leftItem.text}</p>
             </div>
 
-            {/* Sağ Kart */}
-            <div className="bg-white p-6 shadow-lg rounded-lg flex flex-col h-full text-left transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+            <div className="bg-white p-6 shadow-lg rounded-lg flex flex-col h-full text-left transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
               <h2 className="text-2xl font-bold text-gray-900 mb-2">
                 {categoryData.right[index].title}
               </h2>
